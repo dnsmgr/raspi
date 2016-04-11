bc72121575de24c3df162c5631c1d319521b2eca
Christian Aeschlimann <dnsmgr@hotmail.com>
2016-04-11 13:15:20 -0700

    Changed something

---
+++ alexa/creds.py
@@ -0,0 +1,13 @@
+# RENAME THIS FILE TO creds.py before use!
+import os
+
+#Alexa Settings
+ProductID = "RaspberryEcho"
+Security_Profile_Description ="RaspberryEcho"
+Security_Profile_ID  = "amzn1.application.4bd9051f7853409081a769c3f9046adb"
+Client_ID = "amzn1.application-oa2-client.1ca32d1885f6408582033627e77ab277"
+Client_Secret = "58717a52a7274c3ee69ba5c856811076e89b04170b92b347a8d08c7e0918dbce"
+
+# Alexa Refresh Token
+refresh_token = "Atzr|IQEBLjAsAhRrXLvUdnCgTgwb9iTqb_8-0eSVSgIULj53ecMeAlwhql-1xm0XOBGioRF5SP-LXGg2s4v0vR3hWwoe0fI4ENVHtz9DC_l-UxsV1ibh8vsN0IYeQuiFlYrb428z0XpK-ipctSotF3VcK2bZROsrlIBNPKfwh9Ybci0oyly42JkiAU_ZE9LAde6pztuh72aaSs2FrO6Zm8uxEN6uTE5aabE6Cp88a7m-b8klkS_BZ0ilSrbH8scjCevgg7noo7UnONUkv396bQRR_CiJg7iiuVeDdUbGHiYjx5cCbF9AfT9sctLi67AJj1lcelrHlEvgo6XjfdQWCwFqeh6CQbI0o4X3igMwMEAxhGEYriAtXTPXkndBppQkW9sJzwanhFre5iWpXrqrPcbzQPq6q9DL8-os462GWNGPW1HiXbWRxdVmAUdxuaBiDgh2LJAUO1u4TMbRCVhfeV23JCqXNzg3WjD7mwEFniq9aKAtUtlvDtfQz-J_dEz8T0hwU-6Tjxl-jRqoyDXJOg2cBKPM1lXWn4O8Zy-niQEKCuI7hcM"
+
